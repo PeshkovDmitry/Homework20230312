8ed93cd2d0f2ab0bc6f335d258d7af796e7846ba
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-03-12 16:06:20 +0300

    Задача 23

---
+++ task23/Program.cs
@@ -0,0 +1,9 @@
+﻿// задача 23: Напишите программу, которая принимает на вход число (N) 
+// и выдает таблицу кубов чисел от 1 до N  
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{n} -> ");
+for (int i = 1; i <= n; i++ ) {
+    Console.Write($"{Math.Pow(i,3)} ");   
+}
